6423fa72c1812e960e818f24df07e6cca877801f
oli3vares <100429151@alumnos.uc3m.es>
2022-11-16 21:16:35 +0100

    starting the extraction of the values to be returned by the functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 def read_str(ind, list):
     new_operator_list = []
     for i in range(0, 2*len(list), 2):
@@ -44,6 +45,27 @@ if ind[0] in operator:
     operator_list = [root]
     read_str(ind[1:], operator_list)
 
-print(root.data)
-print(root.right.left.data)
+def calculate_function(root, x_value):
+    "No sé si hay que tener en cuenta la jerarquía de las operaciones (creo que no hace falta)"
+    if root.value in operator:
+        if root.value == "*":
+            return multiplication(root.right, root.left, x_value)
+    else:
+        if type(root.value) == int:
+            return root.value
+        else:
+            return x_value
+
+def multiplication(arg1, arg2, x_value):
+    if arg1.value is in operator:
+        "Los ifs"
+        value1 = ...
+    else:
+        value1 = ...
+    if arg2.value is in operator:
+        value2 = ...
+    else:
+        value2 = ...
+    return value1*value2
+
 
