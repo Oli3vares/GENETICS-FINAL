c394bd96054a1ea2b2489efc55e37f3bde545f47
oli3vares <100429151@alumnos.uc3m.es>
2022-11-18 17:12:03 +0100

    should be working with both operators

--- main.py
+++ main.py
@@ -1,26 +1,52 @@
 import csv
-def read_str(ind, list):
+def read_str(ind, list, num_children):
     new_operator_list = []
-    for i in range(0, 2*len(list), 2):
+    children = 0
+    i = 0
+    new_children = 0
+    while children < num_children:
         #se inserta el primer elemento como hijo del nodo x
-        tree.insert_node(list[int((i)/2)], ind[i])
-        #se inserta el segundo elemento como hijo del nodo x
-        tree.insert_node(list[int((i)/2)], ind[i+1])
-        #si alguno de los elementos es un operador, se incluye en la nueva lista
-        if ind[i] in operator:
-            new_operator_list.append(list[int((i)/2)].left)
-        if ind[i+1] in operator:
-            new_operator_list.append(list[int((i)/2)].right)
+        if list[0] in operator_two:
+            tree.insert_node(list[i], ind[children])
+            if ind[children] in operator_two:
+                new_operator_list.append(list[i].left)
+                new_children += 2
+            elif ind[children] in operator_one:
+                new_operator_list.append(list[i].left)
+                new_children += 1
+            children += 1
+            #se inserta el segundo elemento como hijo del nodo x
+            tree.insert_node(list[i], ind[children])
+            if ind[children] in operator_two:
+                new_operator_list.append(list[i].left)
+                new_children += 2
+            elif ind[children] in operator_one:
+                new_operator_list.append(list[i].left)
+                new_children += 1
+            children += 1
+            i += 1
+            #si alguno de los elementos es un operador, se incluye en la nueva lista
+        elif list[0] in operator_one:
+            tree.insert_node(list[i], ind[children])
+            if ind[children] in operator_two:
+                new_operator_list.append(list[i].left)
+                new_children += 2
+            elif ind[children] in operator_one:
+                new_operator_list.append(list[i].left)
+                new_children += 1
+            children += 1
+            i += 1
     if len(new_operator_list) != 0:
         #print("New depth")
-        read_str(ind[2*len(list):], new_operator_list)
+        read_str(ind[2*len(list):], new_operator_list, new_children)
     else:
         return
 
 def calculate_function(root, x_value):
-    "No sé si hay que tener en cuenta la jerarquía de las operaciones (creo que no hace falta)"
-    if root.value in operator:
-        return operator[root.value](root.left, root.right, x_value)
+    if root.value in operator_two.keys():
+        return operator_two[root.value](root.left, root.right, x_value)
+    elif root.value in operator_one.keys():
+        return operator_one[root.value](root.left, x_value)
     else:
         if type(root.value) == int:
             return root.value
@@ -29,15 +55,19 @@ def calculate_function(root, x_value):
 
 
 def get_values(arg1, arg2, x_value):
-    if arg1.value in operator.keys():
-        value1 = operator[arg1.value](arg1.left, arg1.right, x_value)
+    if arg1.value in operator_two.keys():
+        value1 = operator_two[arg1.value](arg1.left, arg1.right, x_value)
+    elif arg1.value in operator_one.keys():
+        return operator_one[arg1.value](root.left, x_value)
     else:
         if arg1.value != 'x':
             value1 = int(arg1.value)
         else:
             value1 = x_value
-    if arg2.value in operator.keys():
-        value2 = operator[arg2.value](arg2.left, arg2.right, x_value)
+    if arg2.value in operator_two.keys():
+        value2 = operator_two[arg1.value](arg1.left, arg1.right, x_value)
+    elif arg2.value in operator_one.keys():
+        value2 = operator_one[arg1.value](root.left, x_value)
     else:
         if arg2.value != 'x':
             value2 = int(arg2.value)
@@ -64,6 +94,9 @@ def division(arg1, arg2, x_value):
     #print("Division:", value1, value2)
     return values[0]/values[1]
 
+def square_root():
+    pass
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -84,7 +117,9 @@ class Tree:
         else:
             root.right = self.create_node(node)
 
-operator = {"+": addition, "-": substraction, "*": multiplication, "/": division}
+operator_two = {"+": addition, "-": substraction, "*": multiplication, "/": division}
+operator_one = {"s": square_root}
+
 
 list_values = []
 file = open('function1.csv',newline='')
@@ -100,7 +135,7 @@ ind2 = list_values[1][0]
 root = Node(ind2[0])
 tree = Tree(root, ind2)
 
-if ind2[0] in operator:
+if ind2[0] in operator_two:
     operator_list = [root]
     read_str(ind2[1:], operator_list)
 
